feb291e94999701f75612ba3d6fc8b85c8699fe0
iarjona <ignacioarjona@gmail.com>
2017-12-21 16:17:10 +0100

    Acceso inicial

--- src/main/java/es/iarjona/controlador/HomeController.java
+++ src/main/java/es/iarjona/controlador/HomeController.java
@@ -1,14 +1,29 @@
 package es.iarjona.controlador;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@Controller
-public class HomeController {
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "es.iarjona.controlador" })
+public class HomeController extends WebMvcConfigurerAdapter {
 
-   // @GetMapping({"/", "/mis-reservas"})
-    public String misReservas(){
-        return "redirect:/mis-reservas/page/1";
+    @Bean
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/seccionAccesos/paginaInicial");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
 
 }
