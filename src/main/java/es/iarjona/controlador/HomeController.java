ffa6a8a633ffdec8d0281bb3a0a2736eda0eca0c
iarjona <ignacioarjona@gmail.com>
2018-01-09 15:56:52 +0100

    configuracion web.xml

--- src/main/java/es/iarjona/controlador/HomeController.java
+++ src/main/java/es/iarjona/controlador/HomeController.java
@@ -1,29 +1,26 @@
 package es.iarjona.controlador;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "es.iarjona.controlador" })
 public class HomeController extends WebMvcConfigurerAdapter {
 
-    @Bean
+/*    @Bean
     public InternalResourceViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/seccionAccesos/paginaInicial");
+        viewResolver.setPrefix("/WEB-INF/html/accesoInicial");
         viewResolver.setSuffix(".html");
         return viewResolver;
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-    }
+        registry.addViewController("/").setViewName("login");
+    }*/
 
 }
